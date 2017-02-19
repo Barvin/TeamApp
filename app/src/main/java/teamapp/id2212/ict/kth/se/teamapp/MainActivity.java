966b50415345fa1aa13472042c8063dc6c373083
agungor <armangungor@gmail.com>
2017-02-19 23:51:35 +0100

    UserProfile and Public Profile

--- app/src/main/java/teamapp/id2212/ict/kth/se/teamapp/MainActivity.java
+++ app/src/main/java/teamapp/id2212/ict/kth/se/teamapp/MainActivity.java
@@ -96,9 +96,11 @@ public class MainActivity extends AppCompatActivity
             Intent intent = new Intent(this, Profile.class);
             startActivity(intent);
         } else if (id == R.id.nav_gallery) {
-
+            Intent intent = new Intent(this, PersonalProfile.class);
+            startActivity(intent);
         } else if (id == R.id.nav_slideshow) {
-
+            Intent intent = new Intent(this, UserProfile.class);
+            startActivity(intent);
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
