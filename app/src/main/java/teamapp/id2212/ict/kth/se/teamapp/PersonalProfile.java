a575aaf59fa8df4e051e9146a90062b0db1a85a0
agungor <armangungor@gmail.com>
2017-02-19 23:55:31 +0100

    PersonalProfile and UserProfile

---
+++ app/src/main/java/teamapp/id2212/ict/kth/se/teamapp/PersonalProfile.java
@@ -0,0 +1,14 @@
+package teamapp.id2212.ict.kth.se.teamapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class PersonalProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_profile);
+    }
+}
