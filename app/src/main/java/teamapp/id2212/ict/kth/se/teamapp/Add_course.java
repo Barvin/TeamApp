0fb95a14b0c2ce57e457ab37b05ef498659c8213
Dima-alissa <dima.alissa@gmail.com>
2017-03-05 18:04:51 +0100

    add_courses

--- app/src/main/java/teamapp/id2212/ict/kth/se/teamapp/Add_course.java
+++ app/src/main/java/teamapp/id2212/ict/kth/se/teamapp/Add_course.java
@@ -2,18 +2,18 @@ package teamapp.id2212.ict.kth.se.teamapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class Add_course extends AppCompatActivity {
+
 
     private FirebaseDatabase mDbcourses;
     private DatabaseReference mRef;
@@ -25,43 +25,25 @@ public class Add_course extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_course);
 
-
         mRef =FirebaseDatabase.getInstance().getReference().child("courses");
         mListView = (ListView) findViewById(R.id.add_course_list_view);
 
-        final ArrayAdapter <String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,mCourses);
-        mListView.setAdapter(arrayAdapter);
-        mRef.addChildEventListener(new ChildEventListener() {
+
+        FirebaseListAdapter<String> firebaseListAdapter = new FirebaseListAdapter<String>(
+                this,
+                String.class,
+                android.R.layout.simple_list_item_1,
+                mRef) {
             @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                String value = dataSnapshot.getValue(String.class);
-                mCourses.add(value);
-                arrayAdapter.notifyDataSetChanged();
+            protected void populateView(View v, String model, int position) {
+                TextView textView = (TextView) v.findViewById(android.R.id.text1);
+                textView.setText(model);
 
             }
 
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
+        };
+        mListView.setAdapter(firebaseListAdapter);
             }
 
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-
-
-    }
 }
+
